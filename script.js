68fda7dfa87daa5da7724ad4b0f59f21468175bd
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-01 13:43:34 +0530

    added addNote and deleteNote function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 class App {
     constructor() {
-        this.notes = [{title: 'Movie', text: 'Justice league'}, {title: 'Learn Javascript', text:'ES6 concepts'}];
+        this.notes = [];
 
         this.form = document.querySelector('#form');
         this.noteTitle = document.querySelector('#note-title');
@@ -14,14 +14,18 @@ class App {
     addEventListeners() {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
+            this.deleteNote(event);
         })
 
         this.form.addEventListener('submit', event => {
             event.preventDefault();
             const title = this.noteTitle.value;
             const text = this.noteText.value;
-            console.log(title, text)
-            this.render();
+            const hasNotes = title || text
+            if(hasNotes) {
+                this.addNote({ title, text })
+            }
+            console.log(this.notes)
         })
     }
 
@@ -45,6 +49,26 @@ class App {
         this.noteText.value = '';
     }
 
+    addNote({ title, text }) {
+        const newNote = {
+            id: this.notes.length > 0 ? this.notes.length + 1 : 1,
+            title,
+            text,
+            color: 'white'
+        }
+        this.notes = [...this.notes, newNote]
+        this.render();
+    }
+
+    deleteNote(event) {
+        event.stopPropagation();
+        if(!event.target.matches('.toolbar-delete')) return
+        const id = event.target.dataset.id
+        console.log(id)
+        this.notes = this.notes.filter(note => note.id !== Number(id));
+        this.render();
+    }
+
     render() {
         this.displayNotes()
     }
@@ -52,8 +76,8 @@ class App {
     displayNotes() {
 
         this.$notes.innerHTML = this.notes.map(note => `
-        <div class="note">
-            <div class="note-title">
+        <div class="note" style="background: ${note.color}" data-id="${note.id}">
+            <div class="${note.title && "note-title"}">
                 ${note.title}
             </div>
             <div class="note-text">
@@ -61,8 +85,8 @@ class App {
             </div>
             <div class="toolbar-container">
                 <div class="toolbar">
-                    <i class="fa-solid fa-palette toolbar-color"></i>
-                    <i class="fa-solid fa-trash toolbar-delete"></i>
+                    <i class="fa-solid fa-trash toolbar-delete" data-id=${note.id}></i>
+                    <i class="fa-solid fa-palette toolbar-color" data-id=${note.id}></i>
                 </div>
             </div>
         </div>
