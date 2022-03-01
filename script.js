470c94ca13345ee4be80e1951ebedcea9de0accd
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-01 11:17:10 +0530

    added display notes function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 class App {
     constructor() {
+        this.notes = [{title: 'Movie', text: 'Justice league'}, {title: 'Learn Javascript', text:'ES6 concepts'}];
+
         this.form = document.querySelector('#form');
         this.noteTitle = document.querySelector('#note-title');
+        this.noteText = document.querySelector('#note-text');
         this.formButtons = document.querySelector('#form-buttons');
+        this.$notes = document.querySelector('#notes');
 
         this.addEventListeners()
     }
@@ -10,6 +14,14 @@ class App {
     addEventListeners() {
         document.body.addEventListener('click', event => {
             this.handleFormClick(event);
+        })
+
+        this.form.addEventListener('submit', event => {
+            event.preventDefault();
+            const title = this.noteTitle.value;
+            const text = this.noteText.value;
+            console.log(title, text)
+            this.render();
         })
     }
 
@@ -29,7 +41,35 @@ class App {
         this.form.classList.remove('form-open');
         this.noteTitle.style.display = 'none';
         this.formButtons.style.display = 'none';
+        this.noteTitle.value = '';
+        this.noteText.value = '';
     }
+
+    render() {
+        this.displayNotes()
+    }
+
+    displayNotes() {
+
+        this.$notes.innerHTML = this.notes.map(note => `
+        <div class="note">
+            <div class="note-title">
+                ${note.title}
+            </div>
+            <div class="note-text">
+                ${note.text}
+            </div>
+            <div class="toolbar-container">
+                <div class="toolbar">
+                    <i class="fa-solid fa-palette toolbar-color"></i>
+                    <i class="fa-solid fa-trash toolbar-delete"></i>
+                </div>
+            </div>
+        </div>
+        `).join('');
+    }
+
+    
 }
 
 new App();
